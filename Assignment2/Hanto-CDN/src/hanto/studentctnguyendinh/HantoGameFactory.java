75c7c1da224750303df491b81a6657f56a73dc09
Cuong Nguyen <nguyentricuong1995@gmail.com>
2016-04-15 00:15:19 -0400

    Move Gamma making code to Gamma class

--- Assignment2/Hanto-CDN/src/hanto/studentctnguyendinh/HantoGameFactory.java
+++ Assignment2/Hanto-CDN/src/hanto/studentctnguyendinh/HantoGameFactory.java
@@ -10,108 +10,71 @@
 
 package hanto.studentctnguyendinh;
 
-import static hanto.common.HantoPieceType.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import hanto.common.HantoGame;
 import hanto.common.HantoGameID;
-import hanto.common.HantoPieceType;
 import hanto.common.HantoPlayerColor;
-import hanto.studentctnguyendinh.common.rule.*;
 import hanto.studentctnguyendinh.alpha.AlphaHantoGame;
 import hanto.studentctnguyendinh.beta.BetaHantoGame;
 import hanto.studentctnguyendinh.gamma.GammaHantoGame;
 
 /**
- * This is a singleton class that provides a factory to create an instance of any version
- * of a Hanto game.
+ * This is a singleton class that provides a factory to create an instance of
+ * any version of a Hanto game.
  * 
  * @author gpollice
  * @version Feb 5, 2013
  */
-public class HantoGameFactory
-{
+public class HantoGameFactory {
 	private static final HantoGameFactory instance = new HantoGameFactory();
-	
+
 	/**
 	 * Default private descriptor.
 	 */
-	private HantoGameFactory()
-	{
+	private HantoGameFactory() {
 		// Empty, but the private constructor is necessary for the singleton.
 	}
 
 	/**
 	 * @return the instance
 	 */
-	public static HantoGameFactory getInstance()
-	{
+	public static HantoGameFactory getInstance() {
 		return instance;
 	}
-	
+
 	/**
 	 * Create the specified Hanto game version with the Blue player moving
 	 * first.
-	 * @param gameId the version desired.
+	 * 
+	 * @param gameId
+	 *            the version desired.
 	 * @return the game instance
 	 */
-	public HantoGame makeHantoGame(HantoGameID gameId)
-	{
+	public HantoGame makeHantoGame(HantoGameID gameId) {
 		return makeHantoGame(gameId, HantoPlayerColor.BLUE);
 	}
-	
+
 	/**
 	 * Factory method that returns the appropriately configured Hanto game.
-	 * @param gameId the version desired.
-	 * @param movesFirst the player color that moves first
+	 * 
+	 * @param gameId
+	 *            the version desired.
+	 * @param movesFirst
+	 *            the player color that moves first
 	 * @return the game instance
 	 */
 	public HantoGame makeHantoGame(HantoGameID gameId, HantoPlayerColor movesFirst) {
 		HantoGame game = null;
 		switch (gameId) {
-			case ALPHA_HANTO:
-				game = new AlphaHantoGame();
-				break;
-			case BETA_HANTO:
-				game = new BetaHantoGame(movesFirst);
-				break;
-			case GAMMA_HANTO:
-				game = makeGammaHantoGame(movesFirst); 
-				break;
+		case ALPHA_HANTO:
+			game = new AlphaHantoGame();
+			break;
+		case BETA_HANTO:
+			game = new BetaHantoGame(movesFirst);
+			break;
+		case GAMMA_HANTO:
+			game = new GammaHantoGame(movesFirst);
+			break;
 		}
 		return game;
-	}
-	
-	private HantoGame makeGammaHantoGame(HantoPlayerColor movesFirst) {
-		HantoRule[] rules = {
-				new HantoRuleGameOver(), 
-				new HantoRuleFirstMoveAtOrigin(),
-				new HantoRuleInputConsistency(),
-				new HantoRuleMoveBeforeButterfly(),
-				new HantoRuleOccupiedHex(),
-				new HantoRuleNotAdjacent(),
-				new HantoRulePiecesQuota(),
-				new HantoRuleButterflyInFourMoves(),
-				new HantoRuleAdjacentSameColor(),
-				new HantoRuleContinuousMove()
-		};
-		
-		HantoEndRule[] endRules = {
-				new HantoEndRuleButterflyIsSurrounded(),
-				new HantoEndRuleMaxNumberOfMoves()
-		};
-		HantoRuleValidator gammaRuleValidator = new HantoRuleValidatorImpl(
-				new ArrayList<HantoRule>(Arrays.asList(rules)),
-				new ArrayList<HantoEndRule>(Arrays.asList(endRules)));
-		
-		Map<HantoPieceType, Integer> gammaPiecesQuota = new HashMap<>();
-		gammaPiecesQuota.put(BUTTERFLY, 1);
-		gammaPiecesQuota.put(SPARROW, 5);
-		
-		return new GammaHantoGame(movesFirst, gammaRuleValidator, gammaPiecesQuota);
 	}
 }
--- Assignment2/Hanto-CDN/src/hanto/studentctnguyendinh/gamma/GammaHantoGame.java
+++ Assignment2/Hanto-CDN/src/hanto/studentctnguyendinh/gamma/GammaHantoGame.java
@@ -12,84 +12,98 @@
 
 package hanto.studentctnguyendinh.gamma;
 
+import static hanto.common.HantoPieceType.BUTTERFLY;
+import static hanto.common.HantoPieceType.SPARROW;
 import static hanto.common.MoveResult.OK;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
-import hanto.common.HantoCoordinate;
-import hanto.common.HantoException;
-import hanto.common.HantoGame;
-import hanto.common.HantoPiece;
-import hanto.common.HantoPieceType;
-import hanto.common.HantoPlayerColor;
-import hanto.common.MoveResult;
+import hanto.common.*;
 import hanto.studentctnguyendinh.HantoPieceFactory;
 import hanto.studentctnguyendinh.common.piece.HantoPieceAbstract;
-import hanto.studentctnguyendinh.common.rule.HantoRuleValidator;
+import hanto.studentctnguyendinh.common.rule.*;
 
 /**
  * A concerete implementation of the Gamma version of the Hanto game.
+ * 
  * @author Cuong Nguyen
  * @version April 7, 2016
  */
-public class GammaHantoGame implements HantoGame
-{
+public class GammaHantoGame implements HantoGame {
 	private GammaHantoGameState gameState;
 	private HantoRuleValidator ruleValidator;
 	private HantoPieceFactory pieceFactory;
-	
-//	public GammaHantoGame(HantoRuleValidator ruleValidator, Map<HantoPieceType, Integer> piecesQuota) {
-//		this(BLUE, ruleValidator, piecesQuota);
-//	}
+
+	// public GammaHantoGame(HantoRuleValidator ruleValidator,
+	// Map<HantoPieceType, Integer> piecesQuota) {
+	// this(BLUE, ruleValidator, piecesQuota);
+	// }
 
 	/**
-	 * Construct a BetaHantoGame instance with the player who moves first being specified.
-	 * @param movesFirst color of the player who moves first
-	 * @param ruleValidator a validators that validates set of rules for this game.
-	 * @param piecesQuota number of available pieces for each piece types.
+	 * Construct a BetaHantoGame instance with the player who moves first being
+	 * specified.
+	 * 
+	 * @param movesFirst
+	 *            color of the player who moves first
+	 * @param ruleValidator
+	 *            a validators that validates set of rules for this game.
+	 * @param piecesQuota
+	 *            number of available pieces for each piece types.
 	 */
-	public GammaHantoGame(HantoPlayerColor movesFirst, HantoRuleValidator ruleValidator, Map<HantoPieceType, Integer> piecesQuota) {
-		this.ruleValidator = ruleValidator;
-		gameState = new GammaHantoGameState(movesFirst, piecesQuota);
+	public GammaHantoGame(HantoPlayerColor movesFirst) {
 		pieceFactory = HantoPieceFactory.getInstance();
-	}	
-	
+		HantoRule[] rules = { new HantoRuleGameOver(), new HantoRuleFirstMoveAtOrigin(),
+				new HantoRuleInputConsistency(), new HantoRuleMoveBeforeButterfly(), new HantoRuleOccupiedHex(),
+				new HantoRuleNotAdjacent(), new HantoRulePiecesQuota(), new HantoRuleButterflyInFourMoves(),
+				new HantoRuleAdjacentSameColor(), new HantoRuleContinuousMove() };
+
+		HantoEndRule[] endRules = { new HantoEndRuleButterflyIsSurrounded(), new HantoEndRuleMaxNumberOfMoves() };
+		ruleValidator = new HantoRuleValidatorImpl(new ArrayList<HantoRule>(Arrays.asList(rules)),
+				new ArrayList<HantoEndRule>(Arrays.asList(endRules)));
+
+		Map<HantoPieceType, Integer> gammaPiecesQuota = new HashMap<>();
+		gammaPiecesQuota.put(BUTTERFLY, 1);
+		gammaPiecesQuota.put(SPARROW, 5);
+
+		gameState = new GammaHantoGameState(movesFirst, gammaPiecesQuota);
+	}
+
 	/*
-	 * @see hanto.common.HantoGame#makeMove(hanto.common.HantoPieceType, hanto.common.HantoCoordinate, hanto.common.HantoCoordinate)
+	 * @see hanto.common.HantoGame#makeMove(hanto.common.HantoPieceType,
+	 * hanto.common.HantoCoordinate, hanto.common.HantoCoordinate)
 	 */
 	@Override
-	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
-			HantoCoordinate to) throws HantoException
-	{
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from, HantoCoordinate to)
+			throws HantoException {
 		ruleValidator.validateRules(gameState, pieceType, from, to);
-		
+
 		if (from == null) {
 			HantoPiece newPiece = pieceFactory.makeHantoPiece(gameState.getCurrentPlayer(), pieceType);
 			gameState.putPieceAt(to, newPiece);
-		}
-		else {
-			HantoPieceAbstract piece = (HantoPieceAbstract)gameState.getPieceAt(from);
+		} else {
+			HantoPieceAbstract piece = (HantoPieceAbstract) gameState.getPieceAt(from);
 			piece.validateMove(gameState, from, to);
 			gameState.movePiece(from, to);
 		}
-		
+
 		gameState.advanceMove();
-		
+
 		MoveResult moveResult = ruleValidator.validateEndRules(gameState);
 		if (moveResult != OK) {
 			gameState.flagGameOver();
 		}
-		
+
 		return moveResult;
 	}
-	
-	
+
 	/*
 	 * @see hanto.common.HantoGame#getPieceAt(hanto.common.HantoCoordinate)
 	 */
 	@Override
-	public HantoPiece getPieceAt(HantoCoordinate where)
-	{
+	public HantoPiece getPieceAt(HantoCoordinate where) {
 		return gameState.getPieceAt(where);
 	}
 
@@ -97,10 +111,8 @@ public class GammaHantoGame implements HantoGame
 	 * @see hanto.common.HantoGame#getPrintableBoard()
 	 */
 	@Override
-	public String getPrintableBoard()
-	{
+	public String getPrintableBoard() {
 		return gameState.getPrintableBoard();
 	}
 
-	
 }
